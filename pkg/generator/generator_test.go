0bb37b71a3517119279d440f55cb1acdddf3568e
Ludovico Russo <ludus.russo@gmail.com>
2022-01-23 14:38:55 +0100

    cleanup package

--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -1,4 +1,4 @@
-package generator
+package generator_test
 
 import (
 	"strings"
@@ -6,19 +6,20 @@ import (
 
 	_ "embed"
 
+	"github.com/livefun/codege-test/pkg/generator"
 	"github.com/matryer/is"
 )
 
-//go:embed example.go.txt
+//go:embed tests/example.go.txt
 var example string
 
-//go:embed example_result.go.txt
+//go:embed tests/example_result.go.txt
 var example_result string
 
 func TestGenerator(t *testing.T) {
 	is := is.New(t)
 	inputReader := strings.NewReader(example)
-	res, err := RunGenerator(inputReader, "main")
+	res, err := generator.RunGenerator(inputReader, "main")
 
 	is.NoErr(err)
 	is.Equal(res, example_result)
